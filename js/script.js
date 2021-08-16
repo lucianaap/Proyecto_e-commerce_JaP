115954f189eb0b4275f01f2994f5034c7ae11b51
lucianaap <lucianaarmandpilon@outlook.com>
2021-08-16 19:02:47 -0300

    carga datos productos JSON

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function cargarDatos (url){
+    document.getElementById("carga_productos").innerHTML = "";
+
+    fetch(url)
+    .then (respuesta => respuesta.json())
+    .then (datos => {
+        datos.forEach(element => {
+            let row = "";
+            row =`
+            <h3> ` + element.name + `</h3>
+            <p> ` + element.description + `</p> 
+            <p> `+ element.cost + ` </p>
+            <img src=" `+ element.imgSrc +`">
+            `;
+            document.getElementById("carga_productos").innerHTML += row;
+        });
+    })
+    .catch(error => alert("Hubo un error: " + error));
+}
