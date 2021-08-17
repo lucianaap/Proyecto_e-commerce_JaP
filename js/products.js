0f6e9f82f0973f6c5979b4064f93202e58256ea2
lucianaap <lucianaarmandpilon@outlook.com>
2021-08-16 23:08:38 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,6 +1,39 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+var currentProductsArray = [];
 
+function showProductsList (){
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let category = currentProductsArray[i];
+
+        if (((minCount == undefined) || (minCount != undefined && parseInt(category.productCount) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))){
+
+            htmlContentToAppend += `
+            <a href="category-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ products.name +`</h4>
+                            <small class="text-muted">` + category.productCount + ` artículos</small>
+                        </div>
+                        <p class="mb-1">` + products.description + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+        }
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL)
+    .then ()
 });
