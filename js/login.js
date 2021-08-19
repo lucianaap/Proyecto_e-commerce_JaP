818ff8c9975f5b7ac2bd8c771ebbcceb52a7812f
lucianaap <lucianaarmandpilon@outlook.com>
2021-08-19 00:06:12 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,3 +1,15 @@
+function loginIngresarRedireccionar(email, password) {
+    if (email.trim()==="" || password.trim()==="") {
+        alert ("Hay campos que debe llenar para ingresar")
+
+    }
+
+
+    location.href="index.html";
+}
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -6,7 +18,5 @@ document.addEventListener("DOMContentLoaded", function(e){
 });
 
 
-function loginRedirect() {
-    location.href="index.html";
-}
+
 
