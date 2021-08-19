49a815527746114d63f963f267f68135137a53aa
lucianaap <lucianaarmandpilon@outlook.com>
2021-08-19 01:56:25 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,18 +1,24 @@
 var productsArray = [];
-
+var imagenesArray = [];
 function showProductsList(productsArray) {
     let list = "<hr>"
     for (let i = 0; i< productsArray.length; i++){
+        
         let autos = productsArray[i];
+        
         list +=  autos.name + "<br>";
         list +=  autos.description + "<br>";3
         list += autos.cost + "<br>";
         list += autos.currency + "<br>";
+        list += `<img id="imagen-producto" src=" `+ autos.imgSrc + `"alt = "`+ autos.description + `" + "<br>" ` + "<br>"
         list += "<br><hr><br>"
+    
     };
+    
     document.getElementById("lista-productos").innerHTML =list;
    
 };
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
