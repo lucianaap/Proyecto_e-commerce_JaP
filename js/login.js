e4e845966175ecd89d278c8ced668e268b501224
lucianaap <lucianaarmandpilon@outlook.com>
2021-08-19 00:33:20 -0300

    update login

--- js/login.js
+++ js/login.js
@@ -1,4 +1,13 @@
+let email = document.getElementById("email-form");
+let contraseña = document.getElementById("password-form")
+function loginRedirect (){
+    if (email.value === "" || contraseña.value === ""){
+        alert ("Necesita rellenar los campos vacíos")
+    } else{
+        window.location = "index.html";
+    }
 
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
