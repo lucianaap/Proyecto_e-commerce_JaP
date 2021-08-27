07a910b41c846d7902b969dc9f29181ec3b33f5b
lucianaap <lucianaarmandpilon@outlook.com>
2021-08-27 11:13:46 -0300

    update

--- js/login.js
+++ js/login.js
@@ -6,7 +6,30 @@ function loginRedirect (){
     } else{
         window.location = "inicio.html";
     }
+}
 
+function saveUsuario() {
+    if (email.value === ""){
+        alert ("No hay datos para guardar")
+    }
+    let usuario = {
+        correo: document.getElementById("email-form").value,
+    };
+    let usuario_json = JSON.stringify(usuario)
+    localStorage.setItem("usuario", usuario_json);
+}
+function recuperarUsuario(){
+    if (localStorage.getItem("usuario")) {
+        usuario_json = localStorage.getItem("usuario");
+    
+    usuario = JSON.parse(usuario_json)
+
+    document.getElementById("usuario").innerHTML = 
+    "Usuario: " + usuario.correo
+    
+}else{
+    document.getElementById("usuario").innerHTML = "No hay datos almacenados"
+}
 }
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
